68b1a42839bbae0958beef46c10b11999815b57c
mn4ben <mb.barhoom@gmail.com>
2025-02-28 21:20:22 +0300

    first edition

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
-        # If the DB file doesn't exist, make a new DB. 
-        # Comment this line out if you want the DB to restart on every launch. 
-        # if not os.path.exists(os.path.join(app.instance_path, "flaskr.sqlite")):
         db.init_db()
     app.run(debug=True)
+    
+    
